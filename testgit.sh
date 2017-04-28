e4e0dcaed06ae5ce4294659312c2125aa9bdfbf7
Abhishek Kandala <abhishek.kandala@gmail.com>
2017-04-28 14:29:00 -0700

    test

--- testgit.sh
+++ testgit.sh
@@ -1,19 +1,17 @@
 echo 'Created by Abhishek Kandala'
 
 git add .
-
 git status
 
 echo 'Enter the commit message:'
 read commitMessage
 
 git commit -m "$commitMessage"
-
 git diff
 
 echo 'Enter the name of the branch:'
 read branch
 
-git pull && push origin $branch
+git push origin $branch
 
 read
